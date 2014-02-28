b2e51c2dc03f614f9456037ec107cd045e4f8321
Marcus Müller <marcus@hostalia.de>
2014-02-28 10:08:29 +0100

    state tested against GNU Radio tree + license notice + minimal docstring for class

---
+++ include-guard-convert.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import re
+import os
+import subprocess
+import shlex 
+
+regexes = {
+		'ifndef' : '^\s*#(?:ifndef|IFNDEF)\s+([A-Z_0-9]{4,})\s*$',
+		'define' : '^\s*#(?:define|DEFINE)\s+([A-Z_0-9]{4,})\s*$',
+		'endif' : '^\s*#(?:endif|ENDIF)\s*(/\*.*\*/|//.+)?\s*$',
+		'blank' : '^\s*(/\*.*\*/|//.+)?\s*$',
+		'pragma' : '^\s*#(?:pragma|PRAGMA)\s+(?:once|ONCE)'
+}
+patterns = dict( [ (key, re.compile(regexes[key]) ) for key in regexes.keys() ] )
+
+cpp_commands = {
+		'strip_comments' : 'cpp -dD  -fpreprocessed  -P -x c++ {file}',
+		'test_if_guarded' : 'cpp -P -x c++ -D{define} {file}'
+}
+
+class guarded_include(object):
+
+	def __init__(self, filename):
+		self.filename = filename
+		assert(self._test_readable())
+
+
+	def _test_readable(self):
+		return os.access(self.filename, os.R_OK)
+
+
+	def test_oldstyle_guarded(self):
+		try:
+			self._stripped = subprocess.check_output(
+					shlex.split(
+							cpp_commands['strip_comments'].format(file=self.filename)
+							),
+					stderr=subprocess.STDOUT
+					)
+			lineend = self._stripped.find('\n')
+			match_ifndef = patterns['ifndef'].search(self._stripped[:lineend])
+			if not match_ifndef:
+					return False
+			define = match_ifndef.group(1)
+			with_define = subprocess.check_output(
+					shlex.split(
+							cpp_commands['test_if_guarded'].format(file=self.filename, define=define)
+							),
+					stderr=subprocess.STDOUT
+					)
+			if not len(with_define) < 2:
+				return False
+			fh = open(self.filename, 'r')
+			line = fh.readline()
+			while not patterns['ifndef'].search(line):
+				line = fh.readline()
+				if not len(line):
+					fh.close()
+					return False
+
+			line = fh.readline()
+			fh.close()
+			return patterns['define'].search(line)
+
+		except subprocess.CalledProcessError as err:
+			return False
+
+	def convert(self):
+		freadh = open(self.filename, 'r')
+		lines = freadh.readlines()
+		freadh.close()
+		fwriteh = open(self.filename , 'w')
+		for l_number in range(1,len(lines)):
+			line = lines[-l_number]
+			if patterns['blank'].search(line):
+				continue
+			elif patterns['endif'].search(line):
+				lines.pop(-l_number)
+				break
+			else:
+				raise SyntaxError('encountered meaningful line after last #endif: \n'+line)
+		define = None
+		done = False
+		for l_number,line in enumerate(lines):
+			if done:
+				fwriteh.write(line)
+				continue
+			if define is None:
+				pattern = patterns['ifndef']
+			else:
+				pattern = patterns['define']
+			match = pattern.search(line)
+			if match:
+				newdefine = match.group(1)
+				if define is None:
+					define = newdefine
+				elif define == newdefine:
+					fwriteh.write('#pragma once\n')
+					done = True
+			else:
+				fwriteh.write(line)
+		if define is None:
+			raise SyntaxError('could not find #ifndef')
+		elif define != newdefine:
+			raise SyntaxError('found #ifndef ' + define + ', does not match #define ' + newdefine)
+		fwriteh.close()
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('filename', nargs='*')
+	args = parser.parse_args()
+	for filen in args.filename:
+		try:
+			gi = guarded_include(filen)
+			if gi.test_oldstyle_guarded():
+				gi.convert()
+		except SyntaxError as e:
+			print filen
+			print e
